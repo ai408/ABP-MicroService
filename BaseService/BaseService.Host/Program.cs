5eeca9fc857d15338c9c1dec0d182f4aa0b094c9
王升升 <913292836@qq.com>
2021-04-11 13:02:52 +0800

    增加Docker.json文件

--- BaseService/BaseService.Host/Program.cs
+++ BaseService/BaseService.Host/Program.cs
@@ -16,6 +16,7 @@ namespace BaseService
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
+                // .AddJsonFile("appsettings.Docker.json")
                 .AddEnvironmentVariables()
                 .Build();
 
--- Gateways/WebAppGateway/WebAppGateway.Host/Program.cs
+++ Gateways/WebAppGateway/WebAppGateway.Host/Program.cs
@@ -15,7 +15,8 @@ namespace WebAppGateway
         {
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
+                // .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Docker.json")
                 .AddEnvironmentVariables()
                 .Build();
 
