b3e71f81160c02a8ab1730e8c4b354cd3cce49d3
王升升 <913292836@qq.com>
2021-04-13 23:20:38 +0800

    修改Business本地运行配置文件

--- MicroServices/Business/Business.Host/Program.cs
+++ MicroServices/Business/Business.Host/Program.cs
@@ -16,6 +16,7 @@ namespace Business
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
+                //.AddJsonFile("appsettings.Docker.json")
                 .AddEnvironmentVariables()
                 .Build();
 
