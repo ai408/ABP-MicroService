c7df722167b3a0e03b5d3fc6ed9e7f1e2fa49ac6
ai408 <913292836@qq.com>
2021-08-05 00:01:43 +0800

    默认加载侧边栏Logo

--- WebApp/vue/src/layout/components/Sidebar/Logo.vue
+++ WebApp/vue/src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: '系统登录',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '工业互联网App平台',
+      //logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
 }
